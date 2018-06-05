9cea53658068702a62928074a068901479dc5839
1400956 <1400956@50HW013371.SOAM.TCS.com>
2018-06-05 17:58:06 -0500

    Se comenta Código de ruta página

--- RetoFinalCursoSelenium/src/test/java/RetoSelenium/RetoFinalCursoSelenium/Test/MainTest.java
+++ RetoFinalCursoSelenium/src/test/java/RetoSelenium/RetoFinalCursoSelenium/Test/MainTest.java
@@ -25,6 +25,7 @@ public class MainTest {
 		driver.manage().window().maximize();
 
 		driver.get("http://www.generatedata.com/?lang=es");
+		//Ruta para paginas descargada
 		//driver.get("file:///D:/SeleniumEjemplos/REto%20Curso%20Selenium/PaginaFormulario/generate.php.html");
 	}
 	
